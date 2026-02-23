7287a7f5b23b70caccfa0c02b102ad0349e57e80
MAjavanta <ma.javanta@gmail.com>
2026-02-23 16:43:03 +0000

    Add manifest json and icon to compile to chrome extension

--- script.js
+++ script.js
@@ -2,20 +2,45 @@ let myTabs = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const tabLst = document.getElementById("tab-lst");
+const deleteBtn = document.getElementById("delete-btn");
+
+const tabsFromLocalStorage = JSON.parse(localStorage.getItem("tabs"));
+if (tabsFromLocalStorage) {
+  myTabs = tabsFromLocalStorage;
+  renderAllLeads();
+}
 
 inputBtn.addEventListener("click", () => {
   myTabs.push(inputEl.value);
+  localStorage.setItem("tabs", JSON.stringify(myTabs));
+  console.log(localStorage.getItem("tabs"));
   renderLead();
   inputEl.value = "";
 });
 
+deleteBtn.addEventListener("dblclick", () => {
+  myTabs = [];
+  localStorage.setItem("tabs", JSON.stringify(myTabs));
+  tabLst.innerHTML = "";
+});
+
 function renderLead() {
-  let listItem = `
-    <li>
-      <a target="_blank" href="${inputEl.value}">
-          ${inputEl.value}
-      </a>
-    </li>`;
-  console.log(listItem);
+  let listItem = getListItemHTML(inputEl.value);
   tabLst.innerHTML += listItem;
+}
+
+function renderAllLeads() {
+  for (let i = 0; i < myTabs.length; i++) {
+    tabLst.innerHTML += getListItemHTML(myTabs[i]);
+  }
+}
+
+function getListItemHTML(link) {
+  return `
+    <li>
+      <a target="_blank" href="${link}">
+        ${link}
+      </a>
+    </li>
+    `;
 }
