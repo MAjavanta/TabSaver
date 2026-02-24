b5e8b5d25e5d0705c906c23b2baf107732ea6199
MAjavanta <ma.javanta@gmail.com>
2026-02-24 11:56:01 +0000

    Add saving tab feature

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const tabLst = document.getElementById("tab-lst");
 const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 
 const tabsFromLocalStorage = JSON.parse(localStorage.getItem("tabs"));
 if (tabsFromLocalStorage) {
@@ -11,10 +12,7 @@ if (tabsFromLocalStorage) {
 }
 
 inputBtn.addEventListener("click", () => {
-  myTabs.push(inputEl.value);
-  localStorage.setItem("tabs", JSON.stringify(myTabs));
-  console.log(localStorage.getItem("tabs"));
-  renderLead();
+  addItem(inputEl.value);
   inputEl.value = "";
 });
 
@@ -24,8 +22,21 @@ deleteBtn.addEventListener("dblclick", () => {
   tabLst.innerHTML = "";
 });
 
-function renderLead() {
-  let listItem = getListItemHTML(inputEl.value);
+tabBtn.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    addItem(tabs[0].url);
+  });
+});
+
+function addItem(item) {
+  myTabs.push(item);
+  localStorage.setItem("tabs", JSON.stringify(myTabs));
+  console.log(localStorage.getItem("tabs"));
+  renderLead(item);
+}
+
+function renderLead(item) {
+  let listItem = getListItemHTML(item);
   tabLst.innerHTML += listItem;
 }
 
